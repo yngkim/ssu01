1ba464fb8f151bef28a4ab7f64aebd59b5575c2c
MJCHO <t9100@naver.com>
2019-05-30 01:26:50 +0900

    adding mv

--- sokoban.c
+++ sokoban.c
@@ -6,26 +6,37 @@
 	/*
 	 *  Sokoban / 19.05.30
 	 *
-	 *	Who made? Mj Cho, Dn Seo, Gw Lee, Yg Kim.
+	 *	Who made? Mj Cho, Dn Seo, Gw Lee, Yg
+ Kim.
 	 *
-	 *	@:Player,  #:Wall,  $:Gold,  O:Safe,  .:Blank
+	 *	@:Player,  #:Wall,  $:Gold,  O:S
+afe,  .:Blank
 	 *
 	*/
 
 
-int x=0, y=0, isOnGame = 0;
+int x = 0, y = 0, lvl = 0, isOnGame = 0;
 int allMap[5][31][31] = {0};
 int curMap[31][31] = {0};
 
 int getch(void);							// 엔터없이 입력
 int getMap(void);							// 맵불러오기
-void setMap(int lvl, char n[11]);			// 맵 출력
-int onGame(int lvl, char name[11]);			// 게임 
+void setMap(char n[12]);					// 맵 출력
+int onGame(char name[12]);					// 게임 
+
+// 해야할것
+void undo(void);
+void save();
+void load();
+void display(void);
+void top(int);
+void mv(int ch, char name[12]);
+//
+
 
 int main(void){
 
-	char player_name[11] = {0};
-	int level = 1;
+	char player_name[12] = {0};
 
 	system("clear");
 
@@ -38,9 +49,9 @@ int main(void){
 	// Get a name
 	printf("input name : ");
 	scanf("%10s", player_name);
-	
+
 //	do{
-		onGame(level, player_name);	
+		onGame(player_name);	
 //	}while(level < 6);
 
 	end :
@@ -57,42 +68,22 @@ int getch(void){
 
 	tcgetattr(0, &save);
 	buf = save;
+
+	buf.c_lflag&=~(ICANON|ECHO);
 	buf.c_cc[VMIN] = 1;
 	buf.c_cc[VTIME] = 0;
 
+	tcsetattr(0, TCSAFLUSH, &buf);
+	
+	ch = getchar();
 	tcsetattr(0, TCSAFLUSH, &save);
 
 	return ch;
 }
 
-void getKey(void){
-	int key;
-
-	key = getch();
-
-	switch(key){
-		case 'h' :
-		case 'j' :
-		case 'k' :
-		case 'l' :
-		case 'u' :
-		case 'r' :
-		case 'n' :
-		case 'e' :
-		case 's' :
-		case 'f' :
-		case 'd' :
-		case 't' :
-		default : break;
-	}
-
-	return;
-}
-
-
 int getMap(void){
 	
-	int c, lvl = 0, i = 0, j = 0, cmp = 0;
+	int c, lvls = 0, i = 0, j = 0, cmp = 0;
 
 	FILE *ifp;
 	
@@ -108,7 +99,7 @@ int getMap(void){
 			switch(c){
 				case '\n' :
 					if(j != 0){                     // 스테이지가 바뀌지 않았을 때 
-						allMap[lvl][i++][j] = c;
+						allMap[lvls][i++][j] = c;
 						j = 0;
 					}
 			 		break;
@@ -118,9 +109,9 @@ int getMap(void){
 				case '3' :
 				case '4' :
 				case '5' :
-					lvl++; i=0; j=0;
+					lvls++; i=0; j=0;
 					if(cmp != 0){                   // 금과 금고개수 확인
-						printf("Error : [Stage-%d] The number of $ and O is different!\n", lvl+1);
+						printf("Error : [Stage-%d] The number of $ and O is different!\n", lvls+1);
 						system("cat >> eLog <<EOF\nError : The number of $ and O is different!\nEOF");
 						fclose(ifp);
 						return 1;
@@ -135,20 +126,20 @@ int getMap(void){
 						cmp++;
 					else if(c == 'O')
 						cmp--;
-					allMap[lvl][i][j++] = c;
+					allMap[lvls][i][j++] = c;
 					break;
 			}
 		}
 	}
 	fclose(ifp);
 
-	printf("\nDone\n");
+	printf("\nDone\n\n");
  		
 	return 0;
 }
 
 
-void setMap(int lvl, char n[11]){
+void setMap(char n[12]){
 	
 	int i,j;
 	
@@ -173,10 +164,46 @@ void setMap(int lvl, char n[11]){
 	
 	printf("\n\n\n(Command) ");
 	return;
+
 }
 
+void mv(int ch, char name[12]){
+	int tmpx = x, tmpy = y;
+	char isS[12] = {'$'};
+	switch(ch){
+		case 'h' : tmpx--; break;
+		case 'l' : tmpx++; break;
+		case 'j' : tmpy++; break;
+		case 'k' : tmpy--; break;
+		default : break;
+	}
+	
+	switch(curMap[tmpy][tmpx]){
+		case '.' : 
+		case 'O' : 
+			if(name[10] != '$'){
+				curMap[tmpy][tmpx]='@';
+				if(allMap[lvl][y][x]=='@')
+					curMap[y][x]='.';
+				else
+					curMap[y][x]=allMap[lvl][y][x];
+			}
+			else
+				curMap[tmpy][tmpx]='$';
+			break;
+		case '$' : 
+			if(name[10] != '$')
+				mv(ch, isS);
+			break;
+		case '#' :
+		default : break;
+	}
+	
+	setMap(name);
 
-int onGame(int lvl, char name[11]){
+	return;
+}
+int onGame(char name[12]){
 	
 	int steps = 0;
 
@@ -184,14 +211,34 @@ int onGame(int lvl, char name[11]){
 
 	for(int i = 0; i < 31; i++){
 		for(int j = 0; j < 31; j++){
-			curMap[i][j]=allMap[lvl-1][i][j];
+			curMap[i][j]=allMap[lvl][i][j];
 		}
 	}
 
+	setMap(name);
+
+
 	do{
-		setMap(lvl, name);
-		printf("%d\n",steps++);
-		getKey();
+		int key;
+		
+		key = getch();
+
+		switch(key){
+			case 'h' :
+			case 'j' :
+			case 'k' :
+			case 'l' : steps++; mv(key, name); break; 	//이동
+			case 'u' : break;
+			case 'r' : break;
+			case 'n' : break;
+			case 'e' : break;
+			case 's' : break;
+			case 'f' : break;
+			case 'd' : break;
+			case 't' : break;
+			default : break;
+		}
+
 	}while(isOnGame == 1);
 
 	return steps;
